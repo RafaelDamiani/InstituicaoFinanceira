01fceb0d2b70c77551b55c6c66f57a2e954306c8
Matheus Leonardo Cabral <ml.cabral@outlook.com>
2019-12-02 23:27:16 -0300

    Terminei a tela de conta

--- src/view/ContaView.java
+++ src/view/ContaView.java
@@ -6,6 +6,18 @@
 package view;
 
 import java.awt.Color;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.DefaultComboBoxModel;
+import model.Cliente;
+import model.ContaCorrente;
+import model.ContaInvestimento;
+import model.dao.ConnectionFactoryComProperties;
+import model.dao.ContaDao;
 
 /**
  *
@@ -18,6 +30,28 @@ public class ContaView extends javax.swing.JPanel {
      */
     public ContaView() {
         initComponents();
+        
+        Connection con = null;
+        PreparedStatement stmt = null;
+
+        try {
+            con = new ConnectionFactoryComProperties().getConnection();
+            List<String> strList = new ArrayList<String>();
+
+            String query = "SELECT * FROM tb_cliente";
+            stmt = con.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                strList.add(rs.getString("nome") + " " + rs.getString("sobrenome"));
+            }
+            stmt.close();
+            con.close();
+            DefaultComboBoxModel defaultComboBoxModel = new DefaultComboBoxModel(strList.toArray());
+            jComboCliente.setModel(defaultComboBoxModel);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     /**
@@ -61,6 +95,11 @@ public class ContaView extends javax.swing.JPanel {
         jLabelClienteConta3.setText("Número da Conta: ");
 
         jButtonSalvarClienteConta.setText("Salvar");
+        jButtonSalvarClienteConta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSalvarClienteContaActionPerformed(evt);
+            }
+        });
 
         inputClienteConta1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         inputClienteConta1.setText("0.00");
@@ -170,12 +209,6 @@ public class ContaView extends javax.swing.JPanel {
     private void jComboContaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboContaActionPerformed
         String conta = (String) jComboConta.getSelectedItem();
         if(conta.equals("Conta Investimento")){
-            /*jLabelDepositoInicial.setVisible(false);
-            depositoInicial.setVisible(false);
-            jLabelLimite.setVisible(false);
-            limite.setVisible(false);
-            jLabelNumeroConta.setVisible(false);
-            numeroConta.setVisible(false);*/
             jLabelClienteConta1.setText("Montante Mínimo:  R$");
             jLabelClienteConta2.setText("Depósito Mínimo:   R$");
             jLabelClienteConta3.setText("Depósito Inicial:     R$");
@@ -183,12 +216,6 @@ public class ContaView extends javax.swing.JPanel {
             inputClienteConta3.setEditable(true);
             inputClienteConta3.setBackground(Color.WHITE);
         }else{
-            /*jLabelDepositoInicial.setVisible(true);
-            depositoInicial.setVisible(true);
-            jLabelLimite.setVisible(true);
-            limite.setVisible(true);
-            jLabelNumeroConta.setVisible(true);
-            numeroConta.setVisible(true);*/
             jLabelClienteConta1.setText("Depósito Inicial:     R$");
             jLabelClienteConta2.setText("Limite:                 R$");
             jLabelClienteConta3.setText("Número da Conta: ");
@@ -199,8 +226,155 @@ public class ContaView extends javax.swing.JPanel {
     }//GEN-LAST:event_jComboContaActionPerformed
 
     private void inputClienteConta1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputClienteConta1ActionPerformed
-        // TODO add your handling code here:
+        String conta = (String) jComboConta.getSelectedItem();
+        if(conta.equals("Conta Investimento")){
+            jLabelClienteConta1.setText("Montante Mínimo:  R$");
+            jLabelClienteConta2.setText("Depósito Mínimo:   R$");
+            jLabelClienteConta3.setText("Depósito Inicial:     R$");
+            inputClienteConta3.setText("0.00");
+            inputClienteConta3.setEditable(true);
+            inputClienteConta3.setBackground(Color.WHITE);
+        }else{
+            jLabelClienteConta1.setText("Depósito Inicial:     R$");
+            jLabelClienteConta2.setText("Limite:                 R$");
+            jLabelClienteConta3.setText("Número da Conta: ");
+            inputClienteConta3.setEditable(false);
+            inputClienteConta3.setText("");
+            inputClienteConta3.setBackground(Color.LIGHT_GRAY);
+        }
     }//GEN-LAST:event_inputClienteConta1ActionPerformed
+
+    private void jButtonSalvarClienteContaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSalvarClienteContaActionPerformed
+       String conta = (String) jComboConta.getSelectedItem();
+        int idCliente = 0, numConta = 0;
+        String sobrenome = "", cpf = null,nomeCliente = null, sobrenomeCliente = null, rg = null, endereco = null;
+        Double salario = null;
+        if(conta.equals("Conta Corrente")){
+            ContaCorrente cc = new ContaCorrente();
+            String depIn = inputClienteConta1.getText();
+            Double di = Double.parseDouble(depIn);
+            cc.setDepositoInicial(di);
+            cc.setSaldo(di);
+            String limite = inputClienteConta2.getText();
+            Double lim = Double.parseDouble(limite);
+            cc.setLimite(lim);
+            String cliente = (String) jComboCliente.getSelectedItem();
+            String[] textoSeparado = cliente.split(" ");
+            System.out.println(textoSeparado[0] + " " + textoSeparado[1]);
+            String nome = textoSeparado[0];
+            for(int i = 1; i < textoSeparado.length; i++){
+                if(i == (textoSeparado.length - 1)){
+                    sobrenome += textoSeparado[i];
+                }else{
+                    sobrenome += textoSeparado[i] + " ";
+                }
+            }
+            System.out.println(textoSeparado[0]);
+            System.out.println(sobrenome);
+            //Busca id do cliente
+            Connection con = null;
+            PreparedStatement stmt = null;
+            try {
+                con = new ConnectionFactoryComProperties().getConnection();
+
+                String query = "SELECT * FROM tb_cliente WHERE nome = ? AND sobrenome = ?";
+                stmt = con.prepareStatement(query);
+                stmt.setString(1, nome);
+                stmt.setString(2, sobrenome);
+                
+                ResultSet rs = stmt.executeQuery();
+                while(rs.next()){
+                    idCliente = rs.getInt("idcliente");
+                    cpf = rs.getString("cpf");
+                    nomeCliente = rs.getString("nome");
+                    sobrenomeCliente = rs.getString("sobrenome");
+                    rg = rs.getString("rg");
+                    endereco = rs.getString("endereco");
+                    salario = rs.getDouble("salario");
+                }
+
+                Cliente c = new Cliente(nomeCliente, sobrenomeCliente, rg, cpf, endereco, salario);
+                c.setIdCliente(idCliente);
+                cc.setCliente(c);
+                ContaDao cDao = new ContaDao();
+                cDao.insertContaCorrente(cc);
+                
+                //gera o numero da conta
+                query = "SELECT num_conta FROM tb_conta WHERE tipo = ? AND idcliente = ?";
+                stmt = con.prepareStatement(query);
+                stmt.setString(1, "CC");
+                stmt.setInt(2, idCliente);
+                rs = stmt.executeQuery();
+                while(rs.next()){
+                    numConta = rs.getInt("num_conta");
+                }
+                
+                stmt.close();
+                con.close();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            String nc = Integer.toString(numConta);
+            inputClienteConta3.setText(nc);
+        }else{
+            ContaInvestimento ci = new ContaInvestimento();
+            String monMin = inputClienteConta1.getText();
+            Double mm = Double.parseDouble(monMin);
+            ci.setMontanteMinimo(mm);
+            String depMin = inputClienteConta2.getText();
+            Double dm = Double.parseDouble(depMin);
+            ci.setDepositoMinimo(dm);
+            String depIni = inputClienteConta3.getText();
+            Double di = Double.parseDouble(depIni);
+            ci.setDepositoInicial(di);
+            ci.setSaldo(di);
+            String cliente = (String) jComboCliente.getSelectedItem();
+            String[] textoSeparado = cliente.split(" ");
+            System.out.println(textoSeparado[0] + " " + textoSeparado[1]);
+            String nome = textoSeparado[0];
+            for(int i = 1; i < textoSeparado.length; i++){
+                if(i == (textoSeparado.length - 1)){
+                    sobrenome += textoSeparado[i];
+                }else{
+                    sobrenome += textoSeparado[i] + " ";
+                }
+            }
+            System.out.println(nome);
+            System.out.println(sobrenome);
+            //Busca id do cliente
+            Connection con = null;
+            PreparedStatement stmt = null;
+            try {
+                con = new ConnectionFactoryComProperties().getConnection();
+
+                String query = "SELECT * FROM tb_cliente WHERE nome = ? AND sobrenome = ?";
+                stmt = con.prepareStatement(query);
+                stmt.setString(1, nome);
+                stmt.setString(2, sobrenome);
+                
+                ResultSet rs = stmt.executeQuery();
+                while(rs.next()){
+                    idCliente = rs.getInt("idcliente");
+                    cpf = rs.getString("cpf");
+                    nomeCliente = rs.getString("nome");
+                    sobrenomeCliente = rs.getString("sobrenome");
+                    rg = rs.getString("rg");
+                    endereco = rs.getString("endereco");
+                    salario = rs.getDouble("salario");
+                }
+
+                Cliente c = new Cliente(nomeCliente, sobrenomeCliente, rg, cpf, endereco, salario);
+                c.setIdCliente(idCliente);
+                ci.setCliente(c);
+                ContaDao cDao = new ContaDao();
+                cDao.insertContaInvestimento(ci);
+                stmt.close();
+                con.close();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }//GEN-LAST:event_jButtonSalvarClienteContaActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
