39e8e1e2684c05a68908d8eb9cdd3c45e15c12b3
RafaelDamiani <rafael2605@gmail.com>
2019-12-02 23:29:52 -0300

    DAO Manipular, Controller Manipular, Manipular View. Incompletos.

---
+++ src/controller/ManipularController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.sql.SQLException;
+import model.Conta;
+import model.dao.ManipularDao;
+
+/**
+ *
+ * @author rafae
+ */
+public class ManipularController {
+    
+    public Conta prepareGetContaByCpf(String cpf) {
+        try {
+            ManipularDao manipularDao = new ManipularDao();
+            return manipularDao.getContaByCpf(cpf);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doSaque(double saque) {
+        try {
+            
+            Conta conta = new Conta();
+            ManipularDao manipularDao = new ManipularDao();
+            manipularDao.updateSaldo(conta);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
